659eb0261f9c10bce897d92e257f8d291f2d3cd7
tdawidzi <01169122@pw.edu.pl>
2025-03-27 18:42:09 +0100

    Added main.go, added all handlers

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"dictionary-app/config"
+	"dictionary-app/schema"
+	"dictionary-app/utils"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+)
+
+func main() {
+	config, err := config.Load()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = utils.ConnectDB(config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer utils.DB.Close()
+
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		params := graphql.Params{
+			Schema: *schema.Schema,
+			// Request:       r,
+			Context:       context.Background(),
+			OperationName: r.URL.Query().Get("query"),
+		}
+		result := graphql.Do(params)
+		if result.HasErrors() {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("Error: %s", result.Errors)))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+
+		data, err := json.Marshal(result.Data)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("Error marshalling result data: %s", err)))
+			return
+		}
+
+		w.Write(data)
+	})
+	fmt.Println("Server listening on http://localhost:8080/graphql")
+	http.ListenAndServe(":8080", nil)
+}
